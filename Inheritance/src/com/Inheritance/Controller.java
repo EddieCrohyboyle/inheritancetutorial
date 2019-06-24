191c4ca9ad0759d0d2919af3af75c4aac2d36855
eddie o donnell <eddie.odonnelletb@gmail.com>
2019-06-24 15:36:17 +0100

    added as dangerous

--- Inheritance/src/com/Inheritance/Controller.java
+++ Inheritance/src/com/Inheritance/Controller.java
@@ -1,5 +1,6 @@
 package com.Inheritance;
 
+import com.sun.org.apache.bcel.internal.generic.I2F;
 
 public class Controller {
 
@@ -7,7 +8,9 @@ public class Controller {
 	 Pet myPet = new Pet(1, "Mickey","21/06/2019", false, 300);
 	 Pet myDog = new Dog(2, "Pluto","21/06/2019", false, 300);
 
-	 
+	 if(myDog instanceOf Dog) {
+		 (Dog(myDog))
+	 }
 	 System.out.println(myPet.toString());
 	 
 	}
--- Inheritance/src/com/Inheritance/Dog.java
+++ Inheritance/src/com/Inheritance/Dog.java
@@ -1,6 +1,25 @@
 package com.Inheritance;
 
+
 public class Dog extends Pet {
-	
+	private boolean isGuardDog;
+
+	public boolean isGuardDog() {
+		return isGuardDog;
+	}
+
+	public void setGuardDog(boolean isGuardDog){
+		this.isGuardDog = isGuardDog;
+	}
+
+	public Dog() {
+		super();
+	}
+
+	@Override
+	public String toString() {
+		String myString = super.toString() + "Dog [isGuardDog =" + isGuardDog +"]";
+				return myString;
+	}
 
 }
